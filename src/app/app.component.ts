a82908c06ae83efa2f251649f0bcc0e889a039c8
LoLo-12 <pai0102@hotmail.com>
2021-02-18 11:35:51 +0800

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import CryptoJS = require("crypto-js");
 
 @Component({
   selector: "my-app",
@@ -6,8 +7,32 @@ import { Component } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  name = "Angular ";
+  name = "CryptoJS";
+  encrWord = "";
+  encrValue = "";
+  decrWord = "";
+  decrValue = "";
 
-  setEncry(keys, text) {
+  setValue() {
+    this.encrValue = this.encr("r*(^er453", this.encrWord);
+  }
+
+  getValue() {
+    this.decrValue = this.decr("r*(^er453", this.decrWord);
+  }
+
+  private encr(keys, text) {
+    const b64 = CryptoJS.AES.encrypt(text, keys).toString();
+    const e64 = CryptoJS.enc.Base64.parse(b64);
+    const eHex = e64.toString(CryptoJS.enc.Hex);
+    return eHex;
+  }
+
+  private decr(keys, text) {
+    const reb64 = CryptoJS.enc.Hex.parse(text);
+    const bytes = reb64.toString(CryptoJS.enc.Base64);
+    const decrypt = CryptoJS.AES.decrypt(bytes, keys);
+    const plain = decrypt.toString(CryptoJS.enc.Utf8);
+    return plain;
   }
 }
